11d101dd7540a5a76c6d01d395ec9002ebd11b7b
artem popkov <popkov.ad@icloud.com>
2023-11-13 17:41:59 +0300

    GOTOVO)

--- BinaryGame777/Form1.Designer.cs
+++ BinaryGame777/Form1.Designer.cs
@@ -29,22 +29,180 @@ namespace BinaryGame777
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(565, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2^7  128";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 40);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2^6  64";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gray;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 40);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "2^5  32";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Gray;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(240, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 40);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "2^4  16";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Gray;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(300, 5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 40);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "2^3  8";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Gray;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(360, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 40);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "2^2  4";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Gray;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(420, 5);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 40);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "2^1  2";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Gray;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(480, 5);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 40);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "2^0  1";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(697, 388);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(697, 388);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- BinaryGame777/Form1.cs
+++ BinaryGame777/Form1.cs
@@ -18,28 +18,53 @@ namespace BinaryGame777
             InitializeComponent();
         }
 
+        Random rand = new Random();
+        Random rand2 = new Random();
+        string prev = "0"; 
+
+        private string generate(int lvl)
+        {
+            double k;
+            do { k = Math.Pow(2, rand.Next(8)); }
+            while (k.ToString() == prev);
+            switch (lvl)
+            {
+                case 2: k++;
+                    break;
+                case 3: k += Math.Pow(2, rand2.Next(5));
+                    break;
+                case 4: k += rand.Next(256 - Convert.ToInt32(k));
+                    break;
+            };
+            prev = k.ToString();
+            return prev;
+        }
+
+
         const int N = 6;
         const int M = 8;
         Button[,] mas = new Button[N, M];
         Label[,] lbl = new Label[N, 2];
-        int v = 5; // индекс видимого ряда
+        int v = -1; // индекс видимого ряда
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int left = 60, top = 10;
+            int left = 60, top = 50;
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < M; j++)
                 {
                     mas[i, j] = new Button();
-                    mas[i, j].Name = "btn_" + i + "_" + j;
+                    mas[i, j].Name = "btn_" + i + "_" + (7 - j);
                     mas[i, j].Top = top;
                     mas[i, j].Left = left;
-                    mas[i, j].Text = i + "_" + j;
+                    mas[i, j].Text = "0";
                     mas[i, j].Width = 50;
                     mas[i, j].Height = 50;
                     mas[i, j].Click += btn_click;
+                    mas[i, j].Visible = false;
                     Controls.Add(mas[i, j]);
+                    //mas[i, j].BringToFront();
                     left += 60;
                 }
                 lbl[i, 0] = new Label();
@@ -47,7 +72,9 @@ namespace BinaryGame777
                 lbl[i, 0].Top = top + 20;
                 lbl[i, 0].Left = 20;
                 lbl[i, 0].AutoSize = true;
-                lbl[i, 0].Text = i + " 0"; // random.next();
+                lbl[i, 0].Text = generate(y1);
+                lbl[i, 0].Visible = false;
+                //lbl[i, 0].BringToFront();
                 Controls.Add(lbl[i, 0]);
 
                 lbl[i, 1] = new Label();
@@ -55,58 +82,103 @@ namespace BinaryGame777
                 lbl[i, 1].Top = top + 20;
                 lbl[i, 1].Left = mas[i, M - 1].Left + 10 + mas[i, M - 1].Width;
                 lbl[i, 1].AutoSize = true;
-                lbl[i, 1].Text = i + " 1"; // random.next();
+                lbl[i, 1].Text = "0";
+                lbl[i, 1].Visible = false;
+                //lbl[i, 1].BringToFront();
                 Controls.Add(lbl[i, 1]);
                 top += 60;
                 left = 60;
             }
+            label1.Text = "Очки: " + p;
+            pictureBox1.SendToBack();
         }
+
+        int p = 0;
 
         private void btn_click(object sender, EventArgs e)
         {
             int x, y;
+            double k = 0;
             x = Convert.ToInt32((sender as Button).Name.Split('_')[1]);
-            y = Convert.ToInt32((sender as Button).Name.Split('_')[2]);
-            for (int i = x; i < N - 1; i++)
-            {
+            y = 7 - Convert.ToInt32((sender as Button).Name.Split('_')[2]);
+            if (mas[x, y].Text == "0")
+                mas[x, y].Text = "1";
+            else mas[x, y].Text = "0";
                 for (int j = 0; j < M; j++)
                 {
-                    mas[i, j].Text = mas[i + 1, j].Text;
+                    if (mas[x,j].Text == "1")
+                    k += Math.Pow(2, Convert.ToInt32(mas[x, j].Name.Split('_')[2]));
                 }
-                lbl[i, 0].Text = lbl[i + 1, 0].Text;
-                lbl[i, 1].Text = lbl[i + 1, 1].Text;
-            }
-
-            // скрывает ряд кнопок
-            if (v > 0)
+            lbl[x, 1].Text = k.ToString();
+            if (lbl[x, 0].Text == lbl[x, 1].Text)
             {
-                for (int j = 0; j < M; j++)
-                    mas[v, j].Visible = false;
-                lbl[v, 0].Visible = false;
-                lbl[v, 1].Visible = false;
-                v--;
+                for (int i = x; i < N - 1; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        mas[i, j].Text = mas[i + 1, j].Text;
+                    }
+                    lbl[i, 0].Text = lbl[i + 1, 0].Text;
+                    lbl[i, 1].Text = lbl[i + 1, 1].Text;
+                    
+                }
+                p += 100 * y1;
+                //скрывает ряд кнопок
+                if (v >= 0)
+                {
+                    for (int j = 0; j < M; j++)
+                        mas[v, j].Visible = false;
+                    lbl[v, 0].Visible = false;
+                    lbl[v, 1].Visible = false;
+                    v--;
+                }
             }
+            //else
+              //  p -= 25 * y1;
+            label1.Text = "Очки: " + p;
         }
 
         private void Form1_Click(object sender, EventArgs e)
         { // делает видимыми кнопки
+            
+        }
+
+
+        int t = 0;
+        int y1 = 1;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 4000 + rand.Next(200);
             if (v < N - 1)
             {
                 v++;
                 for (int j = 0; j < M; j++)
                 {
-
                     mas[v, j].Visible = true;
                     mas[v, j].Text = "0";
-
-
                 }
                 lbl[v, 0].Visible = true;
                 lbl[v, 1].Visible = true;
-                lbl[v, 0].Text = "0";
-                lbl[v, 1].Text = "1";
-                ActiveForm.Text = v.ToString();
+                lbl[v, 0].Text = generate(y1);
+                lbl[v, 1].Text = "0";
+
+                t++;
+
+                if (t == 10) y1 = 2;
+                else if (t == 20) y1 = 3;
+                else if (t == 30) y1 = 4;
             }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
